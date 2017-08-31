a30aca0d9942c1df121a81d6cffe8dc4ab7c7a1f
Samu Juvonen <samu.juvonen@kirjastot.fi>
2017-08-31 17:39:08 +0300

    Autocomplete widget extension

---
+++ public/js/kifiform-autocomplete-tags.js
@@ -0,0 +1,106 @@
+(function($) {
+  "use strict";
+
+  var KEY_ENTER = 13;
+
+  function append_value(input, value) {
+    if (input.val().length > 0) {
+      value = "," + value;
+    }
+    input.val(input.val() + value);
+  }
+
+  function append_tag(root, container, item) {
+    var tag = $("<span>")
+      .attr("class", "kifiform-tag")
+      .attr("data-value", item.value)
+      .text(item.label);
+
+    var close = $("<button>")
+      .attr("type", "button")
+      .attr("title", Drupal.t("Remove tag"))
+      .text("X")
+      .on("click", function(event) {
+        tag.remove();
+        container.trigger("kififormtagremove", {
+          autocomplete: root,
+          container: container[0],
+          tag: tag[0],
+          item: item
+        });
+      }).appendTo(tag);
+
+    container.append(tag);
+    root.trigger("kififormtaginsert", {
+      autocomplete: root,
+      container: container[0],
+      tag: tag,
+      item: item
+    });
+  }
+
+  function init_tags(root, proxy, container) {
+    root.val().split(/,/).forEach(function(value) {
+      if (value.length > 0) {
+        var item = {
+          value: value,
+          label: value.substr(0, value.indexOf("(")).trim()
+        };
+
+        append_value(proxy, value);
+        append_tag(root, container, item);
+      }
+    });
+
+    root.val("");
+  };
+
+  Drupal.behaviors.kifiFormAutoCompleteTags = {
+    attach: function(context, settings) {
+      var elements = $("input.form-autocomplete")
+        .once("kifiform-autocomplete-tags")
+        .each(function(i, _input) {
+          var input = $(_input);
+
+          var tags = $("<div/>")
+            .attr("class", "kifiform-autocomplete-tags")
+            .insertAfter(input);
+
+          var proxy = $("<input/>")
+            .attr("type", "hidden")
+            .attr("name", _input.name)
+            .insertAfter(input);
+
+          init_tags(input, proxy, tags);
+
+          input
+            .on("autocompleteselect", function(event, ui) {
+              append_value(proxy, ui.item.value);
+              append_tag(input, tags, ui.item)
+            })
+            .on("autocompleteclose", function(event) {
+              this.value = "";
+            })
+            .on("keypress", function(event) {
+              if (event.keyCode == KEY_ENTER && this.value.length > 0) {
+                event.preventDefault();
+
+                append_value(proxy, this.value);
+                append_tag(input, tags, {label: this.value, value: this.value});
+
+                this.value = "";
+              }
+            });
+
+          tags.on("kififormtagremove", function(event, ui) {
+            var values = $.map(tags.children(), function(tag, i) {
+              return tag.dataset.value;
+            });
+
+            proxy.val(values.join(","));
+          });
+        });
+
+    }
+  };
+}(jQuery));
